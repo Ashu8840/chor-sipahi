5cf1714b80bcc17e47608bae38a0138e174e553f
Ayush Tripathi <ayush.bhrg@gmail.com>
2025-11-20 21:52:48 +0530

    save

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,7 +41,7 @@ app.use(
 // CORS
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://chor-sipahi-br9m.vercel.app/",
     credentials: true,
   })
 );
